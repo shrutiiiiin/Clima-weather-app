a50f4817bef71c3528aa936e9ce5a036c29f1ff8
shrutiiiiin <shrutinaik2317@gmail.com>
2024-01-07 22:27:01 +0530

    intial commit #4

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,6 +7,12 @@ class loadingscreen extends StatefulWidget {
 }
 
 class _loadingscreenState extends State<loadingscreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
     LocationPermission permission = await Geolocator.requestPermission();
     Position position = await Geolocator.getCurrentPosition(
@@ -17,26 +23,26 @@ class _loadingscreenState extends State<loadingscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: TextButton(
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.blue[100],
-            ),
-            onPressed: () {
-              getLocation();
-            },
-            child: Text(
-              'Get Location',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontStyle: FontStyle.italic),
-            ),
-          ),
-        ),
-      ),
-    );
+        // body: Center(
+        //   child: Padding(
+        //     padding: const EdgeInsets.all(15.0),
+        //     child: TextButton(
+        //       style: TextButton.styleFrom(
+        //         backgroundColor: Colors.blue[100],
+        //       ),
+        //       onPressed: () {
+        //         getLocation();
+        //       },
+        //       child: Text(
+        //         'Get Location',
+        //         style: TextStyle(
+        //             color: Colors.black,
+        //             fontSize: 30,
+        //             fontStyle: FontStyle.italic),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        );
   }
 }
